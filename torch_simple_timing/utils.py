458660c66c275e3bd278d51ec0304c06c0a485bf
vict0rsch <vsch@pm.me>
2023-04-12 16:37:49 -0400

    add docstrings

--- torch_simple_timing/utils.py
+++ torch_simple_timing/utils.py
@@ -2,16 +2,38 @@ import torch
 import torch.distributed as dist
 
 
-def initialized():
+def initialized() -> bool:
+    """
+    Whether or not distributed training is initialized.
+    ``False`` when not initialized or not available.
+
+    Returns:
+        bool: Distributed training is initialized.
+    """
     return dist.is_available() and dist.is_initialized()
 
 
-def get_world_size():
+def get_world_size() -> int:
+    """
+    Returns the number of processes in the current distributed training.
+    Defaults to 1 in the case of non-distributed training.
+
+    Returns:
+        int: number of processes
+    """
     return dist.get_world_size() if initialized() else 1
 
 
-def synchronize():
+def synchronize() -> None:
+    """
+    Synchronizes:
+        * nothing on CPU
+        * per-GPU CUDA streams
+        * across all processes in distributed training with
+    ``torch.distributed.barrier()``
+    """
+    if not torch.cuda.is_available():
+        return
+    torch.cuda.synchronize()
     if get_world_size() > 1:
         dist.barrier()
-    else:
-        torch.cuda.synchronize()
