de7672cc8569f746011ced4c943c523580c52e26
vict0rsch <vsch@pm.me>
2023-05-01 15:21:21 -0400

    improve docs

--- torch_simple_timing/__init__.py
+++ torch_simple_timing/__init__.py
@@ -19,12 +19,13 @@ In simple terms:
     can focus on readability and not data structures
 
 You can also decorate your functions with :func:`~torch_simple_timing.timeit` to time
-their execution with little code overhead. Then use :func:`~torch_simple_timing.get_global_timer`
-to access the timer it uses and access its :meth:`~torch_simple_timing.timer.Timer.stats` or
-use it manually to time code.
+their execution with little code overhead. Then use
+:func:`~torch_simple_timing.get_global_timer` to access the timer it uses and access
+its :meth:`~torch_simple_timing.timer.Timer.stats` or use it manually to time code.
 
 .. code-block:: python
-    :caption: Example of using :func:`~torch_simple_timing.timeit` to time a function (pseudo-code)
+    :caption: Example of using :func:`~torch_simple_timing.timeit` to time a function
+              (pseudo-code)
 
     from torch_simple_timing import timeit, get_global_timer
 
@@ -52,9 +53,11 @@ use it manually to time code.
 from .clock import Clock  # noqa: F401
 from .timer import Timer
 
-#: The global :class:`~torch_simple_timing.timer.Timer` instance
-#: used by :func:`~torch_simple_timing.timeit` to time functions.
 TIMER = Timer()
+"""
+The global :class:`~torch_simple_timing.timer.Timer` instance
+used by :func:`~torch_simple_timing.timeit` to time functions.
+"""
 
 
 def timeit(name=None, gpu=False, timer=None):
@@ -67,10 +70,13 @@ def timeit(name=None, gpu=False, timer=None):
         from torch_simple_timing import timeit, get_global_timer, reset_global_timer
         import torch
 
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        gpu = device.type == "cuda"
+
         # Use the function name as the timer name
-        @timeit(gpu=True)
+        @timeit(gpu=gpu)
         def train():
-            x = torch.rand(1000, 1000, device="cuda" if torch.cuda.is_available() else "cpu")
+            x = torch.rand(1000, 1000, device=)
             return torch.inverse(x @ x)
 
         # Use a custom name
@@ -88,6 +94,14 @@ def timeit(name=None, gpu=False, timer=None):
             print(timer.display(stats=stats))
 
             reset_global_timer()
+
+    Args:
+        name (str, optional): The name of the timer to use. Defaults to the decorated
+            function's name.
+        gpu (bool, optional): Whether to use GPU timing. Defaults to False.
+        timer (:class:`~torch_simple_timing.timer.Timer`, optional): The timer instance
+            to use. Defaults to ``None``, in which case the global timer instance is
+            used.
 
     """
     if timer is None:
