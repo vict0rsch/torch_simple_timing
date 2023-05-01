deca4fb774a764184a48117c9fb6b592293e6508
vict0rsch <vsch@pm.me>
2023-05-01 14:35:07 -0400

    add decorator

--- torch_simple_timing/__init__.py
+++ torch_simple_timing/__init__.py
@@ -20,4 +20,65 @@ In simple terms:
 
 """
 from .clock import Clock  # noqa: F401
-from .timer import Timer  # noqa: F401
+from .timer import Timer
+
+
+TIMER = Timer()
+
+
+def timeit(name, gpu=False, timer=None):
+    """
+    Decorator to time a function call.
+    Example:
+
+    .. code-block:: python
+
+        from torch_simple_timing import timeit, get_global_timer, reset_global_timer
+        import torch
+
+        @timeit("train", gpu=True)
+        def train():
+            x = torch.rand(1000, 1000, device="cuda" if torch.cuda.is_available() else "cpu")
+            return torch.inverse(x @ x)
+
+        @timeit("test")
+        def test_cpu():
+            return torch.inverse(torch.rand(1000, 1000) @ torch.rand(1000, 1000))
+
+        train()
+        test()
+
+        timer = get_global_timer()
+        stats = timer.stats()
+        print(timer.display(stats=stats))
+
+        reset_global_timer()
+
+
+    """
+    if timer is None:
+        global TIMER
+        timer = TIMER
+
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            with timer.clock(name, gpu=gpu):
+                return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
+
+
+def set_global_timer(timer):
+    global TIMER
+    TIMER = timer
+
+
+def get_global_timer():
+    return TIMER
+
+
+def reset_global_timer():
+    global TIMER
+    TIMER = Timer()
